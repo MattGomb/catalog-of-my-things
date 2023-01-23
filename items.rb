ab6a388e3eb11a74e320b2cd044092a2f2d5711d
Matyas Gombos <gombosmm@gmail.com>
2023-01-23 16:34:30 -0500

    Fix linters

--- items.rb
+++ items.rb
@@ -8,7 +8,7 @@ class Item
     @publish_date = publish_date
     @archived = archived
   end
-  
+
   def move_to_archive
     @archived = true if can_be_archived?
   end
@@ -17,6 +17,7 @@ class Item
 
   def can_be_archived?
     return true if published_date < Date.today - 3652
+
     false
   end
 end
