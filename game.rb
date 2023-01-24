8c4a81fcd395103e472c88547f5eb491cb7545b8
dadadon <davianberoni@yahoo.com>
2023-01-24 12:14:57 -0500

    Add game class

---
+++ game.rb
@@ -0,0 +1,19 @@
+require_relative 'items'
+
+class Game < Item
+    attr_accessor :last_played_at, :multiplayer
+
+  def initialize(id, publish_date, last_played_at, multiplayer: false, archived: false)
+    super(id, publish_date, archived)
+    @last_played_at = last_played_at
+    @multiplayer = multiplayer
+  end
+
+  def can_be_archived?
+    if super && last_played_at < Date.today - 365 * 2
+      return true
+    else
+      return false
+    end
+  end
+end
