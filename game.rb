de1f7c612f5c3facc096aa42ea1c1bf1dc1d16bf
dadadon <davianberoni@yahoo.com>
2023-01-24 12:30:17 -0500

    Fix linter error

--- game.rb
+++ game.rb
@@ -10,7 +10,7 @@ class Game < Item
   end
 
   def can_be_archived?
-    super && last_played_at < Date.today - (365 * 2)
+    if last_played_at < Date.today - (365 * 2)
       true
     else
       false
