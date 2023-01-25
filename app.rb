c47e19e4f5c31e9d2bd09c9e8ebd1906a75ca9ef
dadadon <davianberoni@yahoo.com>
2023-01-25 11:09:49 -0500

    Add options to menu

--- app.rb
+++ app.rb
@@ -2,16 +2,25 @@ require_relative './modules/add_book'
 require_relative './modules/list_books'
 require_relative './modules/add_music_album'
 require_relative './modules/list_music_album'
+require_relative './modules/add_game'
+require_relative './modules/list_game'
 require_relative './data/write_book'
 require_relative './data/read_book'
 require_relative './data/write_music_album'
 require_relative './data/read_music_album'
+require_relative './data/read_game'
+require_relative './data/write_game'
+
 
 class App
   include AddBook
   include ListBooks
   include WriteBook
   include ReadBook
+  include ListGame
+  include AddGame
+  include WriteGame
+  include ReadGame
   include AddMusicAlbum
   include ListMusicAlbums
   include WriteMusicAlbum
@@ -20,7 +29,7 @@ class App
   def initialize
     @books = read_book
     @albums = read_music_album
-    @games = []
+    @games = read_game
     @authors = []
     @labels = []
     @genres = []
@@ -34,7 +43,7 @@ class App
     when '2'
       list_music_albums
     when '3'
-      puts 'This will list the games'
+      list_game
     when '4'
       puts 'This will list the genres'
     when '5'
@@ -46,10 +55,11 @@ class App
     when '8'
       add_music_album
     when '9'
-      puts 'This will add a game'
+      add_game
     else
       write_book
       write_music_album
+      write_game
       puts 'Thanks for using the app'
       exit
     end
