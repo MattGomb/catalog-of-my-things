98a6bbce6b909c5aa5fe54fc97b045767e66396c
dadadon <davianberoni@yahoo.com>
2023-01-25 11:07:11 -0500

    Create list game module

---
+++ modules/list_game.rb
@@ -0,0 +1,16 @@
+require_relative '../game'
+
+module ListGames
+  def list_games
+    @games.each do |game|
+        puts "
+        - Title: #{game.title}
+        Author: #{game.author.last_name}, #{game.author.first_name}
+        Genre: #{game.genre.name}
+        Label: #{game.label.title}
+        Published: #{game.publish_date}
+        Last Play: #{game.last_played_at}
+        Multiplayer: #{game.multiplayer}"
+    end
+  end
+end
